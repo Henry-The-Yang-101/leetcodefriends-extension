50628f89c95e80ad42fa1ec495c6104ad696e1e8
Henry Yang <hhenryyangg@gmail.com>
2025-04-06 16:31:39 -0400

    cleaned up padding issue

--- content.js
+++ content.js
@@ -20,8 +20,8 @@ function addFriendsButton() {
     let div = document.createElement("a");
     div.className = "group relative flex h-8 items-center justify-center rounded p-1 hover:bg-fill-3 dark:hover:bg-dark-fill-3 cursor-pointer";
     div.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" width="20" height="20" class="text-text-secondary dark:text-text-secondary hover:text-text-primary dark:hover:text-text-primary">
-        <path d="M17.6 12.1c1.826 0 3.289 -1.474 3.289 -3.3S19.426 5.5 17.6 5.5s-3.3 1.474 -3.3 3.3 1.474 3.3 3.3 3.3zm-8.8 0c1.826 0 3.289 -1.474 3.289 -3.3S10.626 5.5 8.8 5.5 5.5 6.974 5.5 8.8s1.474 3.3 3.3 3.3zm0 2.2c-2.563 0 -7.7 1.287 -7.7 3.85V20.9h15.4v-2.75c0 -2.563 -5.137 -3.85 -7.7 -3.85zm8.8 0c-0.319 0 -0.682 0.022 -1.067 0.055 1.276 0.924 2.167 2.211 2.167 3.795V20.9h6.6v-2.75c0 -2.563 -5.137 -3.85 -7.7 -3.85z"/>
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" width="22" height="22" class="text-text-secondary dark:text-text-secondary hover:text-text-primary dark:hover:text-text-primary">
+        <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5s-3 1.34-3 3 1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 2.01 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
       </svg>
     `;
     div.title = "Friends";
