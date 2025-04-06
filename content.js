f39f7cb83924f9aae5d08309bfc669674ecbce37
Henry Yang <hhenryyangg@gmail.com>
2025-04-06 17:17:45 -0400

    adjusted variable name

--- content.js
+++ content.js
@@ -17,14 +17,14 @@ function getDirectChild(element, selector) {
 function addFriendsButton() {
   window.onload = async () => {
     console.log("HELLO");
-    let div = document.createElement("a");
-    div.className = "group relative flex h-8 items-center justify-center rounded p-1 hover:bg-fill-3 dark:hover:bg-dark-fill-3 cursor-pointer";
-    div.innerHTML = `
+    let friendsButton = document.createElement("a");
+    friendsButton.className = "group relative flex h-8 items-center justify-center rounded p-1 hover:bg-fill-3 dark:hover:bg-dark-fill-3 cursor-pointer";
+    friendsButton.innerHTML = `
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" width="22" height="22" class="text-text-secondary dark:text-text-secondary hover:text-text-primary dark:hover:text-text-primary">
         <path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5s-3 1.34-3 3 1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 2.01 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/>
       </svg>
     `;
-    div.title = "Friends";
+    friendsButton.title = "Friends";
 
     const popup = document.createElement("div");
     // Set popup styling with opacity transition for smooth fade in/out (position will be set dynamically)
@@ -44,14 +44,14 @@ function addFriendsButton() {
     document.body.appendChild(popup);
 
     // Use opacity transition on hover instead of display toggling
-    div.addEventListener("mouseenter", () => {
-        const rect = div.getBoundingClientRect();
+    friendsButton.addEventListener("mouseenter", () => {
+        const rect = friendsButton.getBoundingClientRect();
         popup.style.top = rect.bottom + window.scrollY + 'px';
         popup.style.left = rect.left + window.scrollX + rect.width / 2 + 'px';
         popup.style.transform = 'translateX(-50%)';
         popup.style.opacity = "1";
     });
-    div.addEventListener("mouseleave", () => {
+    friendsButton.addEventListener("mouseleave", () => {
         popup.style.opacity = "0";
     });
 
@@ -69,7 +69,7 @@ function addFriendsButton() {
       let profileContainer = profileButton.closest("div[style*='position: relative']");
       if (profileContainer) {
         // Insert the new element before the profile container
-        container.insertBefore(div, profileContainer);
+        container.insertBefore(friendsButton, profileContainer);
       } else {
         console.warn("Profile container not found!");
       }
