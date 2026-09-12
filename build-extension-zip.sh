373fb58de9e4ceffe7274298f633f9bcb154080a
Henry Yang <hhenryyangg@gmail.com>
2026-09-12 12:50:49 -0400

    updated build script

--- build-extension-zip.sh
+++ build-extension-zip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 version="$(node -p "require('./manifest.json').version")"
+echo "Version found: ${version}"
 zip -rFS "versions/leetcodefriends-v${version}.zip" manifest.json background.js content.js match.js popup_content.html socket.io.min.js submission_interceptor.js username_obtainer.js icons
+echo "Succesfully created package versions/leetcodefriends-v${version}.zip"
