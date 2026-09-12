2b7f95c75a2590e03672573691de4a72dcac9bb6
Henry Yang <hhenryyangg@gmail.com>
2026-09-12 12:42:04 -0400

    add extension packaging script

---
+++ build-extension-zip.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+zip -rFS versions/leetcodefriends-v1.1.2.zip manifest.json background.js content.js match.js popup_content.html socket.io.min.js submission_interceptor.js username_obtainer.js icons
